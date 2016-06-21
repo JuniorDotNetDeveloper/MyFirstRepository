0c42a1f95c0708459678af6c091a5c6237afe441
vadim.abdullaev <vadim.abdullaev@MDDSK40046>
2016-06-21 17:43:53 +0300

    rename the User2.cs to User.cs

--- FirstProjectForGit/FirstProjectForGit/Model/User.cs
+++ FirstProjectForGit/FirstProjectForGit/Model/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FirstProjectForGit.Model
 {
-    public class User2
+    public class User
     {
         public int UserID { get; set; }
         public string FirstName { get; set; }
