368e4c3d313e2c17bbc43a163b9e1f2e8aa5fef3
vadim.abdullaev <vadim.abdullaev@MDDSK40046>
2016-06-22 15:15:16 +0300

    edit User.cs

--- FirstProjectForGit/FirstProjectForGit/Model/User.cs
+++ FirstProjectForGit/FirstProjectForGit/Model/User.cs
@@ -11,7 +11,7 @@ namespace FirstProjectForGit.Model
         public int UserID { get; set; }
         public string FirstName { get; set; }
         public string  LastName { get; set; }
-        public int Age { get; set; }
+     
 
         public void ShowTime()
         {
