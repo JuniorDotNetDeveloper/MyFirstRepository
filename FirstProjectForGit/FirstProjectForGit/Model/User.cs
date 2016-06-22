fedc254d28dcee9aa353062e3e9dbc545359f209
vadim.abdullaev <vadim.abdullaev@MDDSK40046>
2016-06-22 15:18:37 +0300

    asd

--- FirstProjectForGit/FirstProjectForGit/Model/User.cs
+++ FirstProjectForGit/FirstProjectForGit/Model/User.cs
@@ -16,7 +16,7 @@ namespace FirstProjectForGit.Model
         public void ShowTime()
         {
             Console.WriteLine(DateTime.Now);
-            Console.ReadKey();
+           
         
         }
 
